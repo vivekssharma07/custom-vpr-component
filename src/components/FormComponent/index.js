8a2592252d8d245ea8c5aa550af906ffe7f73b07
vivekssharma07@gmail.com <sharma@Viveks-MacBook-Air.local>
2024-05-01 14:45:25 +0530

    Reconstructng form

--- src/components/FormComponent/index.js
+++ src/components/FormComponent/index.js
@@ -3,14 +3,17 @@ import { Input, Checkbox, Dropdown, Button, FormField, FlowLayout, FormFieldLabe
 import { DateInput } from '@salt-ds/lab';
 
 const FormComponent = ({ parameters, onSubmit }) => {
+  // State to manage form data
   const [formData, setFormData] = useState(parameters);
 
+  // Handler function to update form data on change
   const handleChange = (parameterName, value) => {
     setFormData(prevState => {
       const updatedParameters = prevState.map(param => {
         if (param.parameter.parameterName === parameterName) {
           return {
             ...param,
+            // Update isSelected property based on the selected value
             values: param.values.map(item => ({
               ...item,
               isSelected: item.parameterValue === value
@@ -23,45 +26,54 @@ const FormComponent = ({ parameters, onSubmit }) => {
     });
   };
 
-  const isSelectedUserDefined = (options) => options.some(option => option.displayName === "User Defined" && option.isSelected);
+  // Render Dropdown component with default selected value
+  const renderDropdown = (param) => {
+    // Find the default selected option
+    const defaultSelectedOption = param.values.find(option => option.isSelected);
+    const enableDropwDown = formData.find(item => item.type === "dropDown" && item.parameter.parameterName === "sampleParameter")?.values.some(option => option.isSelected) || false;
+    return (
+      <Dropdown
+        valueToString={(option) => option.displayName || [defaultSelectedOption?.displayName]}
+        defaultSelected={defaultSelectedOption ? [defaultSelectedOption.displayName] : []}
+        onSelectionChange={(e, selectedOption) => handleChange(param.parameter.parameterName, selectedOption[0].parameterValue)}
+      >
+        {param.values.map((option) => (
+          <Option key={option.parameterValue} value={option}>
+            {option.displayName}
+          </Option>
+        ))}
+      </Dropdown>
+    );
+  };
 
-  const renderDropdown = (param) => (
-    <Dropdown
-      valueToString={(option) => option.displayName}
-      onSelectionChange={(e, selectedOption) => handleChange(param.parameter.parameterName, selectedOption[0].parameterValue)}
-    >
-      {param.values.map((option) => (
-        <Option key={option.parameterValue} value={option}>
-          {option.displayName}
-        </Option>
-      ))}
-    </Dropdown>
-  );
+  // Render DateInput component if User Defined option is selected
+  const renderDateInput = (param) => {
+    const defaultSelectedOption = param.values.find(option => option.isSelected);
+    return (<DateInput label={param.parameter.displayName} value={defaultSelectedOption?.parameterValue}
+      onChange={(e) => handleChange(param.parameter.parameterName, e.target.value)} />)
+  }
 
-  const renderDateInput = (param) => isSelectedUserDefined(param.values) ? (
-    <DateInput label={param.parameter.displayName} />
-  ) : null;
-
+  // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
     const reqData = formatValue();
     onSubmit(reqData);
   };
 
+  // Format form data for submission
   const formatValue = () => {
     const profileId = 12345;
     const parameterToUpdate = [];
     const parentParameters = {};
 
     formData.forEach(param => {
-      if (param.dynamic && param.dynamic.parents.includes("multiView")) {
-        parameterToUpdate.push(param.parameter.parameterName);
-      }
-      if (param.dynamic && param.dynamic.parents.includes("sampleParameter")) {
-        const values = param.values.filter(value => value.isSelected);
-        if (values.length > 0) {
+      // Check if dynamic parents include either "sampleParameter" or "multiView"
+      if (param.dynamic && (param.dynamic.parents.includes("sampleParameter") || param.dynamic.parents.includes("multiView"))) {
+        const selectedValues = param.values.filter(value => value.isSelected);
+        if (selectedValues.length > 0) {
           const parameterName = param.parameter.parameterName;
-          parentParameters[parameterName] = values.map(value => ({ parameterValue: value.parameterValue }));
+          parameterToUpdate.push(parameterName);
+          parentParameters[parameterName] = selectedValues.map(value => ({ parameterValue: value.parameterValue }));
         }
       }
     });
@@ -76,19 +88,23 @@ const FormComponent = ({ parameters, onSubmit }) => {
           <FormFieldLabel>{param.parameter.displayName}</FormFieldLabel>
           <StackLayout gap={1} direction="row" role="group">
             {param.type === 'dateRule' ? (
+              // Render Dropdown and DateInput for 'dateRule' type
               <>
                 {renderDropdown(param)}
                 {renderDateInput(param)}
               </>
             ) : param.type === 'dropDown' ? (
+              // Render Dropdown for 'dropDown' type
               renderDropdown(param)
             ) : param.type === 'text' ? (
+              // Render Input for 'text' type
               <Input
                 type="text"
                 value={formData[param.parameter.parameterName] || ''}
                 onChange={(e) => handleChange(param.parameter.parameterName, e.target.value)}
               />
             ) : param.type === 'Checkbox' ? (
+              // Render Checkbox for 'Checkbox' type
               <StackLayout gap={1} direction="row">
                 {param.values.map(item => (
                   <Checkbox
@@ -100,6 +116,7 @@ const FormComponent = ({ parameters, onSubmit }) => {
                 ))}
               </StackLayout>
             ) : param.type === 'Radiobutton' ? (
+              // Render RadioButton for 'Radiobutton' type
               <StackLayout gap={1}>
                 {param.values.map(item => (
                   <RadioButton
