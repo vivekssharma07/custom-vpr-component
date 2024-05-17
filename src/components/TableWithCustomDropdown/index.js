42abfd939b7e04bdf4dff1c74a7a21895c1805a5
vivekssharma07@gmail.com <sharma@Viveks-MacBook-Air.local>
2024-05-17 15:42:31 +0530

    cell render

--- src/components/TableWithCustomDropdown/index.js
+++ src/components/TableWithCustomDropdown/index.js
@@ -1,42 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { StackLayout, Input, Button } from '@salt-ds/core';
-import { CheckboxRenderer } from './CheckboxRenderer';
 
 export const TableWithCustomDropdown = ({ param, handleChange }) => {
   const [gridApi, setGridApi] = useState(null);
   const [rowData, setRowData] = useState([]);
   const [newItemName, setNewItemName] = useState('');
+  const [isCheckBoxSelected, setCheckboxSelected] = useState('');
+
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      headerName: "Select All",
+      field: "displayName",
+      headerCheckboxSelection: true,
+      checkboxSelection: true,
+    }
+  ]);
 
   useEffect(() => {
     if (param?.values?.length) {
       setRowData(param.values);
-      // if (gridApi) {
-      //   gridApi.forEachNode(node => {
-      //     if (node.data.isSelected) {
-      //       node.setSelected(true);
-      //     }
-      //   });
-      // }
     }
   }, [param?.values]);
 
-  // Column definitions for ag-Grid
-  const columnDefs = [
-    {
-      headerName: param && param.parameter.displayName,
-      field: 'displayName',
-      checkboxSelection: true,
-      headerCheckboxSelection: true,
-      //floatingFilter: param && param?.parameter?.parameterName === 'inputList',
-      //filter: param && param?.parameter?.parameterName === 'inputList',
-      //cellRenderer: CheckboxRenderer,
-    },
-  ];
+  const onFirstDataRendered = useCallback((params) => {
+    const nodesToSelect = [];
+    params.api.forEachNode((node) => {
+      if (node.data && node.data.isSelected === true) {
+        nodesToSelect.push(node);
+      }
+    });
+    params.api.setNodesSelected({ nodes: nodesToSelect, newValue: true });
+  }, []);
 
-  // Add new item to the list
   const addItemToList = () => {
     if (newItemName.trim() !== '') {
       const newItem = { displayName: newItemName, parameterValue: newItemName, isSelected: false };
@@ -46,24 +44,28 @@ export const TableWithCustomDropdown = ({ param, handleChange }) => {
     }
   };
 
-  const handleSelectionChanged = () => {
-    const selectedNodes = gridApi.getSelectedNodes();
+  const handleSelectionChanged = (e) => {
+    console.log("handleSelectionChanged")
+    const selectedNodes = e.api.getSelectedNodes();
     const selectedData = selectedNodes.map(node => node.data);
     if (selectedData) {
       handleChange(param.parameter.parameterName, selectedData)
     }
-  };
+  }
 
   return (
     <div className="ag-theme-alpine" style={{ height: '260px', width: '250px' }}>
       <AgGridReact
-        onGridReady={({ api }) => {
-          setGridApi(api);
-          api.sizeColumnsToFit();
-        }}
+        // onGridReady={({ api }) => {
+        //   //setGridApi(api);
+        //   //api.sizeColumnsToFit();
+        // }}
+        onGridReady={handleGridReady}
+
         columnDefs={columnDefs}
         rowData={rowData}
-        rowSelection="multiple"
+        rowSelection='multiple'
+        onFirstDataRendered={onFirstDataRendered}
         onSelectionChanged={handleSelectionChanged}
       />
       {param?.parameter?.parameterName === 'inputList' && (
@@ -79,3 +81,4 @@ export const TableWithCustomDropdown = ({ param, handleChange }) => {
     </div>
   );
 };
+
